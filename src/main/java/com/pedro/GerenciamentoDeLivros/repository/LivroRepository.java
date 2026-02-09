297dd0c6a7a9402615183079b1cbb25da6a4c57c
Pedro Henrique Lima <pedroconicz@gmail.com>
2026-02-09 20:11:29 -0300

    fix: muda tipo do id de UUID para Long

--- src/main/java/com/pedro/GerenciamentoDeLivros/repository/LivroRepository.java
+++ src/main/java/com/pedro/GerenciamentoDeLivros/repository/LivroRepository.java
@@ -3,7 +3,5 @@ package com.pedro.GerenciamentoDeLivros.repository;
 import com.pedro.GerenciamentoDeLivros.model.LivroModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.UUID;
-
-public interface LivroRepository extends JpaRepository<LivroModel, UUID> {
+public interface LivroRepository extends JpaRepository<LivroModel, Long> {
 }
