2e1ca54d15d0f33ff33fd2fa1d5c9ddbbf8ad54b
Pedro Henrique Lima <pedroconicz@gmail.com>
2026-02-09 16:48:57 -0300

    first commit

---
+++ src/main/java/com/pedro/GerenciamentoDeLivros/GerenciamentoDeLivrosApplication.java
@@ -0,0 +1,13 @@
+package com.pedro.GerenciamentoDeLivros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciamentoDeLivrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciamentoDeLivrosApplication.class, args);
+	}
+
+}
