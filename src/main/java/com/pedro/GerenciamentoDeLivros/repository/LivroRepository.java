7694d5cb9693385cfd22304e48f088df8279bc47
Pedro Henrique Lima <pedroconicz@gmail.com>
2026-02-09 18:49:34 -0300

    feat: cria LivroRepository com JPA

--- src/main/java/com/pedro/GerenciamentoDeLivros/repository/LivroRepository.java
+++ src/main/java/com/pedro/GerenciamentoDeLivros/repository/LivroRepository.java
@@ -1,4 +1,9 @@
 package com.pedro.GerenciamentoDeLivros.repository;
 
-public interface LivroRepository {
+import com.pedro.GerenciamentoDeLivros.model.LivroModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface LivroRepository extends JpaRepository<LivroModel, UUID> {
 }
